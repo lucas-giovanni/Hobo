61be584469cd26d7ee1598c73706df19e56546f3
Lucas Adel <6014652@mborijnland.com>
2022-02-21 12:00:29 +0100

    css. inlog.php en een afbeelding aangemaakt

---
+++ inlog.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css" >
+    <title>Login</title>
+  </head>
+  <body class="inlog-background">
+    <div class="login-block">
+      <h1>Sign In</h1>
+      <div class="inlog-invoer">
+        <p>E-mail Or Phone Number</p>
+        <p>Password</p>
+      </div>
+      <div class="inlog-knop">
+        <a href="#">Sign In</a>
+      </div>
+      <div class="inlog-privacy">
+        <form action="/action_page.php">
+          <input type="checkbox" id="privacy1" name="privacy" value="privacy">
+          <label for="Privacy"> <a href="#">privacy</a> </label><br>
+          <input type="submit" value="Submit">
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
